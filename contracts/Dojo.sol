a9770512805e371725fdd959df578d26d6596ecc
Damien Da Fonseca <da.fonseca.damien@gmail.com>
2022-12-28 23:54:33 +0100

    [UPD] ~ Smart Contract and Test

--- contracts/Dojo.sol
+++ contracts/Dojo.sol
@@ -1,46 +1,57 @@
 pragma solidity ^0.8.17;
 
-// Contract for the Dojo
-// The Dojo is the main contract for the game
-// The Dojo contains the fighters
-// The Dojo is owned by the player
-// The Dojo is the contract to trade Fighters token to other players
-// The Dojo is more popular if the fighters in the Dojo are more powerful
 contract Dojo {
-    // The owner of the Dojo
     address public owner;
+    mapping(address => uint) public numFighters;
+    mapping(address => Fighter[]) public fighters;
 
-    // struct of a Fighter
     struct Fighter {
-        // The level of the Fighter
-        uint256 level;
-        // The abilities of the Fighter
-        uint256[] abilities;
-        // The stats of the Fighter
-        uint256[] stats;
-        // The weapons of the Fighter
-        uint256[] weapons;
+        uint level;
+        uint health;
+        uint lives;
+        uint strength;
+        string name;
     }
 
-    // mapping to store the fighters of the owner
-    mapping(address => Fighter[]) public ownerFighters;
-    // mapping to store the fighters of the owner
-    mapping(address => uint) public ownerFightersCount;
-
-    // The event to notify that a new fighter has been added to the Dojo
-    event FighterAdded(address indexed owner, uint256 indexed fighterId);
-    // The event to notify that a fighter has been removed from the Dojo
-    event FighterRemoved(address indexed owner, uint256 indexed fighterId);
-    // The event to notify that a fighter has been traded to another player
-    event FighterTraded(address indexed from, address indexed to, uint256 indexed fighterId);
-
-    constructor () {
+    constructor() {
         owner = msg.sender;
-        ownerFightersCount[owner] = 0;
     }
 
-    // get the number of fighters of the owner
-    function getOwnerFightersCount(address _owner) public view returns (uint) {
-        return ownerFightersCount[_owner];
+    function createFighter(uint _level, uint _health, uint _lives, uint _strength, string memory _name) public {
+        Fighter memory fighter = Fighter(_level, _health, _lives, _strength, _name);
+        fighters[msg.sender].push(fighter);
+        numFighters[msg.sender]++;
+    }
+
+    //TODO: Implement the function, better verification of the fighter Owner
+    function exchangeFighters(address _from, address _to) public {
+        require(numFighters[_from] > 0, "The sender has no fighters");
+        require(numFighters[_to] > 0, "The receiver has no fighters");
+
+        Fighter memory fighter = fighters[_from][0];
+        fighters[_from][0] = fighters[_to][0];
+        fighters[_to][0] = fighter;
+    }
+
+    //TODO: Implement the function
+    function combat(address _address1, uint _index1, address _address2, uint _index2) public {
+        require(_address1 != _address2, "Cannot fight with the same address");
+        require(_index1 < numFighters[_address1], "Invalid index for fighter from");
+        require(_index2 < numFighters[_address2], "Invalid index for fighter to");
+
+        Fighter storage fighter1 = fighters[_address1][_index1];
+        Fighter storage fighter2 = fighters[_address2][_index2];
+
+        // Modify fighter attributes based on the outcome of the combat
+        // ...
+    }
+
+    function getNumFighters(address _owner) public view returns (uint) {
+        return numFighters[_owner];
+    }
+
+    function getFighter(address _owner, string _name) public view returns (Fighter memory) {
+        require(_index < numFighters[_owner], "Invalid index for fighter");
+        return fighters[_owner][_index];
     }
 }
--- test/Dojo.js
+++ test/Dojo.js
@@ -1,12 +1,14 @@
-const { expect } = require('chai');
-const {ethers} = require("hardhat");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 
-describe("Dojo contract", function () {
+describe("Dojo Smart Contract Test", function () {
+
     async function deployTokenFixture() {
         // Get the ContractFactory and Signers here.
         const Dojo = await ethers.getContractFactory("Dojo");
         const [owner, addr1, addr2] = await ethers.getSigners();
-        // To deploy our contract, we just have to call Dojo.deploy() and await
+        // To deploy our contract, we just have to call Token.deploy() and await
         // its deployed() method, which happens once its transaction has been
         // mined.
         const hardhatDojo = await Dojo.deploy();
@@ -17,13 +19,24 @@ describe("Dojo contract", function () {
 
     describe("Deployment", function () {
         it("Should set the right owner", async function () {
-            const { hardhatDojo, owner } = await deployTokenFixture();
+            const { hardhatDojo, owner } = await loadFixture(deployTokenFixture);
             expect(await hardhatDojo.owner()).to.equal(owner.address);
         });
-        it("should create a Dojo with no Fighters", async function () {
-            const { hardhatDojo, owner } = await deployTokenFixture();
-            const fightersCount = await hardhatDojo.getOwnerFightersCount(owner.address);
-            expect(fightersCount).to.equal(0);
-        });
+    });
+
+    it("should create a new fighter", async function () {
+        const { hardhatDojo, owner, addr1, addr2 } = await loadFixture(deployTokenFixture);
+
+        await hardhatDojo.createFighter(1, 100, 3, 50, "Fighter1", { from: owner.address });
+        const numFighters = await hardhatDojo.getNumFighters(owner.address);
+        expect(numFighters).to.equal(1);
+
+        const fighter = await hardhatDojo.getFighter(owner.address, 0);
+        expect(fighter.level).to.equal(1);
+        expect(fighter.health).to.equal(100);
+        expect(fighter.lives).to.equal(3);
+        expect(fighter.strength).to.equal(50);
+        expect(fighter.name).to.equal("Fighter1");
     });
 });
+
