70a8bdee946366d0c20b350058b4f912ac56b643
Damien Da Fonseca <da.fonseca.damien@gmail.com>
2023-01-13 19:50:09 +0100

    [DELETE] ~ Contract mixin in frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,13 +6,11 @@
 <script lang="ts">
 import Header from "./components/Header.vue";
 import {defineComponent} from "vue";
-import contractMixin from "./utils/contractMixin";
 export default defineComponent({
   name: "App",
   components: {
     Header
   },
-  mixins: [contractMixin],
   data(){
     return {
     }
