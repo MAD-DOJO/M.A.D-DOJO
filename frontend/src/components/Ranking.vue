34cdf64a69f6e6b99e8f6a90707a4611b9b218ca
milutinstanojevic <milutinstan@outlook.fr>
2023-01-18 23:22:45 +0100

    Ranking table and buttons

--- frontend/src/components/Ranking.vue
+++ frontend/src/components/Ranking.vue
@@ -1,13 +1,86 @@
 <template>
-  <div class="grid grid-cols-1 gap-2">
-    <div class="p-4 text-center bg-white border rounded-lg shadow-md sm:p-8 bg-gray-800 border-gray-700 m-5" style="height:75vh;">
-      <h5 class="mb-2 text-3xl font-bold text-gray-900 text-white">Vos combattants</h5>
-      <div class="grid grid-cols-4 gap-4 overflow-auto" style="height:40vh;">
-        <div v-for="fighter in fighters" class="mr-2 mt-2">
-          <FighterCard :fighter="fighter" v-on:click="selectedFighter = fighter" class="hover:bg-gray-600"/>
-        </div>
+  <div class="p-4 text-center bg-white border rounded-lg shadow-md sm:p-8 bg-gray-800 border-gray-700 m-5">
+  <div class="flex" style="justify-content: center;">
+      <div class="m-3">
+        <button class="bg-gray-500 text-white font-semibold py-2 px-4 rounded hover:bg-gray-300"
+                v-on:click="this.selectedRank = 'Beginner'">
+          Beginner
+        </button>
       </div>
-    </div>
+      <div class="m-3">
+        <button class="bg-gray-500 text-white font-semibold py-2 px-4 rounded hover:bg-gray-300"
+                v-on:click="this.selectedRank = 'Novice'">
+        Novice
+        </button>
+      </div>
+      <div class="m-3">
+        <button class="bg-gray-500 text-white font-semibold py-2 px-4 rounded hover:bg-gray-300"
+                v-on:click="this.selectedRank = 'Apprentice'">
+        Apprentice
+        </button>
+      </div>
+      <div class="m-3">
+        <button class="bg-gray-500 text-white font-semibold py-2 px-4 rounded hover:bg-gray-300"
+                v-on:click="this.selectedRank = 'Adept'">
+        Adept
+        </button>
+      </div>
+      <div class="m-3">
+        <button class="bg-gray-500 text-white font-semibold py-2 px-4 rounded hover:bg-gray-300"
+                v-on:click="this.selectedRank = 'Master'">
+        Master
+        </button>
+      </div>
+      <div class="m-3">
+        <button class="bg-gray-500 text-white font-semibold py-2 px-4 rounded hover:bg-gray-300"
+                v-on:click="this.selectedRank = 'GrandMaster'">
+        Grand Master
+        </button>
+      </div>
+      <div class="m-3">
+        <button class="bg-gray-500 text-white font-semibold py-2 px-4 rounded hover:bg-gray-300"
+                v-on:click="this.selectedRank = 'Legendary'">
+        Legendary
+        </button>
+      </div>
+  </div>
+
+  <div class="relative overflow-x-auto ml-10 mr-10 mt-6">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+      <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <tr>
+        <th scope="col" class="px-6 py-3">
+          Name
+        </th>
+        <th scope="col" class="px-6 py-3">
+          Level
+        </th>
+        <th scope="col" class="px-6 py-3">
+          Rank
+        </th>
+        <th scope="col" class="px-6 py-3">
+          Wins
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" v-for="fighter in this.filteredFighters">
+          <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            {{ fighter.name }}
+          </th>
+          <td class="px-6 py-4">
+            {{ fighter.level }}
+          </td>
+          <td class="px-6 py-4">
+            {{ fighter.rank }}
+          </td>
+          <td class="px-6 py-4">
+            {{ fighter.wins }}
+          </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
   </div>
 </template>
 
@@ -27,23 +100,30 @@ name: "Ranking",
     return {
       store: dojoStore(),
       fighters : [],
+      filteredFighters: [],
       selectedFighter: undefined,
       selectedRank: "Beginner",
     }
   },
   methods: {
-  //sortFightersByWins function
+
     sortByWins(){
-      this.fighters.sort((a, b) => (a.wins < b.wins) ? 1 : -1);
+      this.filteredFighters.sort((a, b) => (a.wins < b.wins) ? 1 : -1);
     },
-    //filter By rank Beginner function
+
     filterByRank() {
-      this.fighters = this.fighters.filter(fighter => {
+      this.filteredFighters = this.fighters.filter(fighter => {
             if (fighter.rank === this.selectedRank) {
               return fighter;
             }});
       console.log(this.fighters);
     },
+  },
+  watch: {
+    selectedRank: function () {
+      this.filterByRank();
+      this.sortByWins();
+    }
   },
   beforeMount() {
     this.store.getAllFighters().then((fighters) => {
